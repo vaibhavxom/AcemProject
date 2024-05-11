0d33439f4b96e8fd42ec6bcc45aa9bf64d1883eb
vaibhavxom <rmithagare02@gmail.com>
2024-05-11 15:16:58 +0530

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,18 @@
+ <?php echo file_get_contents("html1/header.html"); 
+?>
+<?php echo file_get_contents("html1/body.html");?>
+<?php echo file_get_contents("html1/footer.html"); ?>
+
+
+
+  <br>
+  <br>
+<p class="text-center">Copyright © Acem Projects <?php echo date("Y"); ?></p> 
+<br>
+  <br>
+  <br>
+  <center>
+  <?php include('count.php'); ?>
+
+  </center>
+
